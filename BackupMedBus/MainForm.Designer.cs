893e0cabc468c262a3ec23f1839839f1a1189f9a
evangelalessandro <evangel.alessandro@gmail.com>
2016-06-13 17:49:43 +0200

    Funzione report

--- BackupMedBus/MainForm.Designer.cs
+++ BackupMedBus/MainForm.Designer.cs
@@ -29,11 +29,45 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.button1 = new System.Windows.Forms.Button();
             this.RtbboxLog = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.imgProgress = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.imgProgress)).BeginInit();
             this.SuspendLayout();
+            // 
+            // RtbboxLog
+            // 
+            this.RtbboxLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RtbboxLog.Location = new System.Drawing.Point(12, 12);
+            this.RtbboxLog.Name = "RtbboxLog";
+            this.RtbboxLog.Size = new System.Drawing.Size(578, 384);
+            this.RtbboxLog.TabIndex = 2;
+            this.RtbboxLog.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Image = global::BackupMedBus.Properties.Resources.Report;
+            this.button2.Location = new System.Drawing.Point(596, 254);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 68);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Report";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // imgProgress
+            // 
+            this.imgProgress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.imgProgress.Location = new System.Drawing.Point(644, 12);
+            this.imgProgress.Name = "imgProgress";
+            this.imgProgress.Size = new System.Drawing.Size(100, 73);
+            this.imgProgress.TabIndex = 3;
+            this.imgProgress.TabStop = false;
             // 
             // button1
             // 
@@ -49,31 +83,12 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // RtbboxLog
-            // 
-            this.RtbboxLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.RtbboxLog.Location = new System.Drawing.Point(12, 12);
-            this.RtbboxLog.Name = "RtbboxLog";
-            this.RtbboxLog.Size = new System.Drawing.Size(578, 384);
-            this.RtbboxLog.TabIndex = 2;
-            this.RtbboxLog.Text = "";
-            // 
-            // imgProgress
-            // 
-            this.imgProgress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.imgProgress.Location = new System.Drawing.Point(644, 12);
-            this.imgProgress.Name = "imgProgress";
-            this.imgProgress.Size = new System.Drawing.Size(100, 73);
-            this.imgProgress.TabIndex = 3;
-            this.imgProgress.TabStop = false;
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(756, 408);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.imgProgress);
             this.Controls.Add(this.RtbboxLog);
             this.Controls.Add(this.button1);
@@ -90,6 +105,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox RtbboxLog;
         private System.Windows.Forms.PictureBox imgProgress;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- BackupMedBus/Properties/Resources.Designer.cs
+++ BackupMedBus/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace BackupMedBus.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Report {
+            get {
+                object obj = ResourceManager.GetObject("Report", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap yellow {
             get {
                 object obj = ResourceManager.GetObject("yellow", resourceCulture);
